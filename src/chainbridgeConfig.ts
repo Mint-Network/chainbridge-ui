c8f97f2c3636a799168c058c38807a3353bd7572
udyankumar30 <48148451+udyankumar30@users.noreply.github.com>
2021-07-21 23:49:33 -0700

    Add files via upload

--- src/chainbridgeConfig.ts
+++ src/chainbridgeConfig.ts
@@ -49,92 +49,48 @@ export type ChainbridgeConfig = {
 
 export const chainbridgeConfig: ChainbridgeConfig = {
   // Local GETH <> Local Substrate
-  chains: [
-    // {
-    //   chainId: 0,
-    //   networkId: 5,
-    //   name: "Ethereum - Local",
-    //   decimals: 18,
-    //   bridgeAddress: "0x62877dDCd49aD22f5eDfc6ac108e9a4b5D2bD88B",
-    //   erc20HandlerAddress: "0x3167776db165D8eA0f51790CA2bbf44Db5105ADF",
-    //   rpcUrl: "http://localhost:8545",
-    //   type: "Ethereum",
-    //   nativeTokenSymbol: "ETH",
-    //   tokens: [
-    //     {
-    //       address: "0x21605f71845f372A9ed84253d2D024B7B10999f4",
-    //       name: "TOKEN",
-    //       symbol: "TOKEN",
-    //       imageUri: ETHIcon,
-    //       resourceId:
-    //         "0x000000000000000000000000000000c76ebe4a02bbc34786d860b355f5a5ce00",
-    //     },
-    //   ],
-    // },
-    // {
-    //   chainId: 1,
-    //   networkId: 2,
-    //   name: "Substrate - Local",
-    //   decimals: 18,
-    //   rpcUrl: "ws://localhost:9944",
-    //   type: "Substrate",
-    //   nativeTokenSymbol: "DOT",
-    //   chainbridgePalletName: "chainBridge",
-    //   bridgeFeeFunctionName: "tokenTransferFee",
-    //   transferPalletName: "example",
-    //   transferFunctionName: "transferNative",
-    //   typesFileName: "bridgeTypes.json",
-    //   tokens: [
-    //     {
-    //       address: "substrate-native",
-    //       name: "DOT",
-    //       symbol: "DOT",
-    //       resourceId: "substrate-native",
-    //     },
-    //   ],
-    // },
-    {
-      chainId: 0,
-      networkId: 42,
-      name: "Ethereum",
-      decimals: 18,
-      bridgeAddress: "0x478ab279Ac5F4bd69382D34cF2382606E6208eFc",
-      erc20HandlerAddress: "0x3483c3a1Af5e78AE5AaB07de3Ea57b6F3877745F",
-      rpcUrl: "wss://kovan.infura.io/ws/v3/e199aa0da7e54bd9be94de96ea753127",
-      type: "Ethereum",
-      nativeTokenSymbol: "ETH",
-      tokens: [
+ chains: [
         {
-          address: "0x2726A258f88b4e5B3a251e3d91594c527E10494D",
-          name: "wCFG",
-          symbol: "wCFG",
-          imageUri: ETHIcon,
-          resourceId:
-            "0x00000000000000000000000000000009e974040e705c10fb4de576d6cc261900",
+            chainId: 0,
+            networkId: 4,
+            name: "Ethereum - Rinkeby",
+            bridgeAddress: "0xE00A714D9819cb1577802493264e264a665D2030",
+            erc20HandlerAddress: "0x53125EF48e1b798645553639561679D2700dA7b0",
+            rpcUrl: "https://rinkeby-light.eth.linkpool.io/",
+            type: "Ethereum",
+            blockExplorer: "https://rinkeby.etherscan.io/tx",
+            nativeTokenSymbol: "ETH",
+            tokens: [
+                {
+                    address: "0xc778417e063141139fce010982780140aa0cd5ab",
+                    name: "Wrapped ETH",
+                    symbol: "WETH",
+                    imageUri: WETHIcon,
+                    resourceId:
+                    "0x000000000000000000000000000000c76ebe4a02bbc34786d860b355f5a5ce00",
+                },
+            ],
         },
-      ],
-    },
-    {
-      chainId: 1,
-      networkId: 2,
-      name: "Centrifuge - Amber",
-      decimals: 18,
-      rpcUrl: "wss://fullnode.amber.centrifuge.io",
-      type: "Substrate",
-      nativeTokenSymbol: "CFG",
-      chainbridgePalletName: "chainBridge",
-      bridgeFeeFunctionName: "tokenTransferFee",
-      transferPalletName: "palletBridge",
-      transferFunctionName: "transferNative",
-      typesFileName: "bridgeTypes.json",
-      tokens: [
         {
-          address: "substrate-native",
-          name: "CFG",
-          symbol: "CFG",
-          resourceId: "substrate-native",
+            chainId: 1,
+            networkId: 1280,
+            name: "TestNet - MintNetwork",
+            bridgeAddress: "0x45b5C191526DeC7be43AbEC26B2F5b99f00742C2",
+            erc20HandlerAddress: "0x3b55137B4d4167B945281dbB7Af87dD694FBD412",
+            rpcUrl: "https://testnet.mintnetwork.org",
+            type: "Ethereum",
+            blockExplorer: "http://18.117.82.231:3000/tx",
+            nativeTokenSymbol: "MNT",
+            tokens: [
+                {
+                    address: "0x1067904A35E504cf4b6a14E1160C35Db10A8F59A",
+                    name: "METH",
+                    symbol: "METH",
+                    imageUri: WETHIcon,
+                    resourceId:
+                    "0x000000000000000000000000000000c76ebe4a02bbc34786d860b355f5a5ce00",
+                },
+            ],
         },
-      ],
-    },
-  ],
+    ],
 };
